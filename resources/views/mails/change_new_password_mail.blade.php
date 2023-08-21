7aa8d4f68b2cbd67e40a56c82006dce30ca3ec3f
Pranay Mondal <pranay.tobai@gmail.com>
2023-08-21 08:14:31 +0530

    Modify Email blade files

--- resources/views/mails/change_new_password_mail.blade.php
+++ resources/views/mails/change_new_password_mail.blade.php
@@ -1,8 +1,8 @@
-<h1 style="color: #ff0066"> Hi {{ $user->first_name }} {{ $user->last_name }}</h1>
-<p style="color: #1500ff">The New Change Password Is given Below</p>
+<h1 style="font-family: 'Water Brush', cursive;"> Hi {{ $user->first_name }} {{ $user->last_name }}</h1>
+<p s >The New Change Password Is given Below</p>
 
-<h2 style="color: #3cca1f"> Your email/ Login Id: '{{ $user->email }}' </h2>
-<h2 style="color: #3cca1f">  Password is: '{{ $password }}' </h2>
-<h2 style="color: #1500ff"> Updated at: '{{ $user->updated_at->format('d/m/Y H:i:s')}}' </h2>
+<h2  > Your email/ Login Id: '{{ $user->email }}' </h2>
+<h2  >  Password is: '{{ $password }}' </h2>
+<h2  > Updated at: '{{ $user->updated_at->format('d/m/Y H:i:s')}}' </h2>
 
-<h1 style="color:#00cc00; font-family: 'Water Brush', cursive;"> So let's Change  </h1>
+<h1 style="font-family: 'Water Brush', cursive;"> So let's Change  </h1>
